1ed1edec81094438bf938083cb9e29d79dc0a097
Alyssa Reyes <alyssamarie_reyes@hotmail.com>
2021-05-03 23:04:13 +0100

    Added github

--- src/App.js
+++ src/App.js
@@ -16,6 +16,27 @@ function App() {
         <Search city="New York" />
         <Forecast />
       </div>
+      <br />
+      <span id="Signature">
+        Created by Alyssa Reyes {""}
+        <a
+          href="https://github.com/Lyching0310/React-Weather-App"
+          target="_blank"
+          rel="noreferrer"
+          id="gitLink"
+        >
+          Open-source code
+        </a>
+        {""} and {""}
+        <a
+          href="https://alyssareactweatherapp.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          id="Netlify"
+        >
+          Hosted on Netlify
+        </a>
+      </span>
     </div>
   );
 }
