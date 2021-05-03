9ec1ff61cd04368add142d0702154f325617f16b
Alyssa Reyes <alyssamarie_reyes@hotmail.com>
2021-05-03 20:25:14 +0100

    Added axios and added weather.json

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather() {
+  function handleResponse(response) {
+    alert(`The weather in London is ${Math.round(response.data.main.temp)}°C`);
+  }
+
+  let apiKey = "aff29a6b33c30edafe99104b632f71d7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h2> New World</h2>;
+}
